b63e9374ca7533d5bb930fbfca334b84044395d1
bismayesh <125033480+bismayesh@users.noreply.github.com>
2023-10-04 17:19:35 +0200

    practicing

--- Assets/Bisma/scripts/Spawner.cs
+++ Assets/Bisma/scripts/Spawner.cs
@@ -7,7 +7,7 @@ public class Spawner : MonoBehaviour
 {
     public GameObject[] stars;
     public Transform[] points;
-    public float beat = (60 / 130);
+    public float beat = (60 / 120);
     private float timer;
 
  
@@ -18,7 +18,7 @@ public class Spawner : MonoBehaviour
         if (timer > beat)
         {
 
-            GameObject star= Instantiate(stars[Random.Range(0, 18)], points[Random.Range(0,8)]);
+            GameObject star= Instantiate(stars[Random.Range(0, 12)], points[Random.Range(0,12)]);
             star.transform.localPosition = Vector3.zero;
             star.transform.Rotate(transform.forward);
             timer -= beat;
