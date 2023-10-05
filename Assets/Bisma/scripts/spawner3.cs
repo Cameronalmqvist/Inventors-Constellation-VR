bbb162ff26bd00b9a1a6e8d6b7ac7a7e6413b324
bismayesh <125033480+bismayesh@users.noreply.github.com>
2023-10-05 14:18:38 +0200

    final script spawner3

---
+++ Assets/Bisma/scripts/spawner3.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class spawner3 : MonoBehaviour
+{
+    public Transform[] points;
+    public GameObject[] stars;
+    private int currentIndex = 0;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnStar", 10f, 5f);
+    }
+
+    void SpawnStar()
+    {
+        if (currentIndex < points.Length)
+        {
+            if (currentIndex < stars.Length)
+            {
+                GameObject newStar = Instantiate(stars[currentIndex], points[currentIndex].position, points[currentIndex].rotation, transform);
+                currentIndex++;
+            }
+            
+        }
+        
+    }
+}
+
+
+
+
+
+
+
