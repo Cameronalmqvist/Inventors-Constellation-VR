ac2876acfd29e2b8204987cccecff9c5f1732696
Cameronalmqvist <118892135+Cameronalmqvist@users.noreply.github.com>
2023-10-06 14:29:53 +0200

    Bubble fixes gaze interaction done

--- Assets/Bisma/scripts/MoveTowardsEmptyObject.cs
+++ Assets/Bisma/scripts/MoveTowardsEmptyObject.cs
@@ -1,20 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class MoveTowardsEmptyObject : MonoBehaviour
 {
-    [SerializeField] private GameObject target; 
-    [SerializeField]public float speed = 5f; 
+    [SerializeField] private GameObject target;
+    [SerializeField] public float speed = 5f;
+
+    private bool shouldMove = false;
+
+    void Start()
+    {
+        XRBaseInteractable interactable = GetComponent<XRBaseInteractable>();
+        if (interactable != null)
+        {
+            interactable.onHoverEntered.AddListener(OnHoverEnter);
+            interactable.onHoverExited.AddListener(OnHoverExit);
+        }
+        else
+        {
+            Debug.LogError("No XRBaseInteractable component found on this GameObject.");
+        }
+    }
 
     void Update()
     {
-        
-        transform.position = Vector3.MoveTowards(transform.position,target.transform.position,speed*Time.deltaTime);
+        if (shouldMove)
+        {
+            MoveTowardsTarget();
+        }
+    }
 
+    private void OnHoverEnter(XRBaseInteractor interactor)
+    {
+        shouldMove = true;
+    }
+
+    private void OnHoverExit(XRBaseInteractor interactor)
+    {
+        shouldMove = false;
+    }
+
+    private void MoveTowardsTarget()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
         transform.up = target.transform.position - transform.position;
-
-    } 
-
+    }
 }
-
