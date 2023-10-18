b5d992cc4b32a6b275729397020f8af7fa3754c3
Cameronalmqvist <118892135+Cameronalmqvist@users.noreply.github.com>
2023-10-18 17:21:46 +0200

    Fixed eye gazing

--- Assets/Bisma/scripts/MoveTowardsEmptyObject.cs
+++ Assets/Bisma/scripts/MoveTowardsEmptyObject.cs
@@ -5,8 +5,11 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class FloatingOrb : MonoBehaviour
 {
     [Header("Movement Parameters")]
+    [SerializeField] private Transform target;
+    public float normalSpeed = 5f;
+    public float maxGazedSpeed = 10.0f;
     public float floatSpeed = 1.0f;
-    public float gazedSpeed = 3.0f;
+    public float minDistanceFromTarget = 0.5f;
 
     private Vector3 floatingDirection;
     private Rigidbody rb;
@@ -23,23 +26,31 @@ public class FloatingOrb : MonoBehaviour
         XRBaseInteractable interactable = GetComponent<XRBaseInteractable>();
         interactable.onHoverEntered.AddListener(OnGazeEnter);
         interactable.onHoverExited.AddListener(OnGazeExit);
+
+        
     }
 
     void Update()
     {
-        
-        if (isGazedUpon)
+        float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+        if (isGazedUpon && distanceToTarget > minDistanceFromTarget)
         {
-            // Move towards player when gazed upon
-            Vector3 directionToPlayer = (Camera.main.transform.position - transform.position).normalized;
-            rb.velocity = directionToPlayer * gazedSpeed;
+            // Calculate the direction from the orb to the player's hand.
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            // Adjust the gazed speed based on distance
+            float adjustedGazedSpeed = Mathf.Lerp(normalSpeed, maxGazedSpeed, distanceToTarget / 10f); // Assuming max distance for max speed is 10 units. Adjust as necessary.
+
+            rb.velocity = directionToTarget * adjustedGazedSpeed;
         }
         else
         {
-            // Regular floating movement
             rb.velocity = floatingDirection * floatSpeed;
         }
     }
+
+
 
     void OnCollisionEnter(Collision collision)
     {
@@ -57,12 +68,14 @@ public class FloatingOrb : MonoBehaviour
 
     void OnGazeEnter(XRBaseInteractor interactor)
     {
+        Debug.Log("Gaze Entered");
         isGazedUpon = true;
     }
 
+
     void OnGazeExit(XRBaseInteractor interactor)
     {
+        Debug.Log("Gaze Exited");
         isGazedUpon = false;
-        PickRandomDirection();  // After stopping the gaze, the orb should float in a new direction
     }
 }
