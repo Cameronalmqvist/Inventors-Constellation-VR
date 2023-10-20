536c8182f55e8b799adfa2b0f8e4bea9e9b33f63
Cameronalmqvist <118892135+Cameronalmqvist@users.noreply.github.com>
2023-10-20 19:46:41 +0200

    Fadetoblack and other lerping changes

--- Assets/Cameron/Scripts/FadeToBlack.cs
+++ Assets/Cameron/Scripts/FadeToBlack.cs
@@ -4,8 +4,15 @@ using System.Collections;
 
 public class FadeToBlack : MonoBehaviour
 {
-    public float fadeDuration = 2.0f;
+    public float fadeDuration = 2.0f;  // Duration of the fade effect in seconds
     private Image image;
+
+    [Header("Objects to Preserve")]
+    public GameObject musicObject;   // Drag your music object here
+    public GameObject uiObject;      // Drag your main UI object here
+    public GameObject xrRigObject;  // Drag your XR rig here
+    public GameObject additionalObject1;  // Drag your first additional object here
+    public GameObject additionalObject2;  // Drag your second additional object here
 
     private void Awake()
     {
@@ -33,5 +40,27 @@ public class FadeToBlack : MonoBehaviour
         // Ensure the image is fully opaque
         color.a = 1f;
         image.color = color;
+
+        // Once the fade in is complete, destroy other objects
+        DestroyOtherObjects();
+    }
+
+    private void DestroyOtherObjects()
+    {
+        // Fetch all active root gameObjects
+        GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+
+        foreach (GameObject go in allObjects)
+        {
+            // Destroy the object if it's not one of the objects to preserve
+            if (go != musicObject && go.transform.root != musicObject.transform &&
+                go != uiObject && go.transform.root != uiObject.transform &&
+                go != xrRigObject && go.transform.root != xrRigObject.transform &&
+                go != additionalObject1 && go.transform.root != additionalObject1.transform &&
+                go != additionalObject2 && go.transform.root != additionalObject2.transform)
+            {
+                Destroy(go);
+            }
+        }
     }
 }
